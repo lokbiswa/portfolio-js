32ce8ddc7a42ea83d561fb27d59c0ab2b09cb302
Lok Biswa <lokbiswa@gmail.com>
2021-10-06 03:51:37 -0500

    feat: added contact form and navlink

--- src/scrollSpy.js
+++ src/scrollSpy.js
@@ -2,22 +2,25 @@ const sections = document.querySelectorAll(".section");
 let navLinks = document.querySelectorAll(".nav-link");
 
 window.onscroll = () => {
-  sections.forEach((sec) => {
-    let fromTop = window.scrollY;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight - 165;
-    let id = sec.id;
-    if (fromTop >= offset && fromTop < offset + height) {
-      navLinks.forEach((link) => {
-        link.classList.remove("active");
-        document
-          .querySelector(`.nav-item a[href ='#${id}']`)
-          .classList.add("active");
-      });
-      navBarPosition(id);
-    }
+  setTimeout(() => {
+    sections.forEach((sec) => {
+      let fromTop = window.scrollY;
+      let offset = sec.offsetTop;
+      let height = sec.offsetHeight - 165;
+      let id = sec.id;
+      if (fromTop >= offset && fromTop < offset + height) {
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          document
+            .querySelector(`.nav-item a[href ='#${id}']`)
+            .classList.add("active");
+        });
+        navBarPosition(id);
+      }
+    }, 1000);
   });
 };
+
 function navBarPosition(id) {
   if (id === sections[1].id) {
     console.log(id);
