203f1150fcaa5ec00c3a226e51bda68b178e87a5
Lok Shankar <lokbiswa@gmail.com>
2021-10-07 11:33:24 -0500

    fixed scrollSpy

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// data
 const name = "Lok";
 const lastname = "Shankar";
 const summary =
@@ -5,29 +6,34 @@ const summary =
 
 const linkedinLink = "https://www.linkedin.com/in/lok-shankar";
 const githubLink = "https://github.com/lokbiswa";
-
+//Data for projects
 const projects = [
   {
     title: "Fun House Pizza",
     link: "https://lokbiswa.github.io/funHousePizza/",
+    github: "https://github.com/lokbiswa/funHousePizza",
     thumbnail: "./asset/funhousepizza.png",
   },
   {
     title: "AMT Mock-up",
     link: "https://lokbiswa.github.io/atm-UsingObject/",
+    github: "",
     thumbnail: "./asset/atm.png",
   },
   {
     title: "TODO LIST",
     link: "https://lokbiswa.github.io/project-5/",
+    github: "",
     thumbnail: "./asset/todo.png",
   },
   {
     title: "FlawNalysis",
     link: "https://flawnalysis.herokuapp.com/",
+    github: "https://github.com/lokbiswa/FlawNalysis",
     thumbnail: "./asset/flawNalysis.png",
   },
 ];
+// dynamically create elements
 function eachProject(object) {
   const projectsDiv = document.querySelector(".projects-row");
   const col = document.createElement("div");
@@ -37,13 +43,30 @@ function eachProject(object) {
   const projectThumbnail = document.createElement("img");
   projectThumbnail.src = object.thumbnail;
   projectThumbnail.className = "img-fluid";
+  // on hover info of each projects
+  const projectInfo = document.createElement("div");
+  projectInfo.className = "info";
+  projectName = document.createElement("h4");
+  projectName.className = "project-names";
+  projectName.innerHTML = object.name;
+  const hostedLink = document.createElement("a");
+  hostedLink.className = "liveDemo";
+  hostedLink.href = object.link;
+  const github = document.createElement("a");
+  github.className = "projectLink";
+  github.href = object.github;
+  // hidden dive appending
+  nestElements(projectInfo, projectName, hostedLink, github);
+  //  appending elements
   nestElements(project, projectThumbnail);
   nestElements(col, project);
   nestElements(projectsDiv, col);
 }
+// render created elements
 function renderProjects(list) {
   list.forEach(eachProject);
 }
+// add content dynamically
 function onPageLoad() {
   renderProjects(projects);
   const linked = document.getElementById("linkedin");
@@ -63,3 +86,11 @@ function nestElements(...elements) {
 function getElementAssignInnerHTML(id, innerHtml) {
   document.getElementById(id).innerHTML = innerHtml;
 }
+// to collapes the navbar.
+const menuToggle = document.getElementById("navbarSupportedContent");
+const bsCollapse = new bootstrap.Collapse(menuToggle);
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    bsCollapse.toggle();
+  });
+});
--- src/scrollSpy.js
+++ src/scrollSpy.js
@@ -1,35 +1,34 @@
 const sections = document.querySelectorAll(".section");
 let navLinks = document.querySelectorAll(".nav-link");
-
+// To set Navbar to page top
 window.onscroll = () => {
-  setTimeout(() => {
-    sections.forEach((sec) => {
-      let fromTop = window.scrollY;
-      let offset = sec.offsetTop;
-      let height = sec.offsetHeight - 165;
-      let id = sec.id;
-      if (fromTop >= offset && fromTop < offset + height) {
-        // navLinks.forEach((link) => {
-        //   link.classList.remove("active");
-        //   document
-        //     .querySelector(`.nav-item a[href ='#${id}']`)
-        //     .classList.add("active");
-        // });
-        navBarPosition(id);
-      }
-    }, 100);
+  sections.forEach((sec) => {
+    let fromTop = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight - 165;
+    let id = sec.id;
+    // check if scroll
+    if (fromTop >= offset && fromTop < offset + height) {
+      // navLinks.forEach((link) => {
+      //   link.classList.remove("active");
+      //   document
+      //     .querySelector(`.nav-item a[href ='#${id}']`)
+      //     .classList.add("active");
+      // });
+      navBarPosition(id);
+    }
   });
 };
-
-// function navBarPosition(id) {
-//   if (id === sections[1].id) {
-//     console.log(id);
-//     const navbar = document.getElementById("navbar");
-//     navbar.classList.add("fix-top");
-//   }
-//   if (id === sections[0].id) {
-//     const navbar = document.getElementById("navbar");
-//     navbar.classList.remove("fix-top");
-//   }
-// }
+// using fix-top to fix navbar on top
+function navBarPosition(id) {
+  if (id === sections[1].id) {
+    console.log(id);
+    const navbar = document.getElementById("navbar");
+    navbar.classList.add("fix-top");
+  }
+  if (id === sections[0].id) {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.remove("fix-top");
+  }
+}
 // navBarPosition("projects");
